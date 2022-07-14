c5979cb045636b7f0f972052bdf6f3f82a653646
JuanjoLv <juanjoleon1999@gmail.com>
2022-07-14 19:22:29 +0200

    Task 020: done

---
+++ src/main/java/acme/entities/systemConfiguration/SystemConfiguration.java
@@ -0,0 +1,38 @@
+package acme.entities.systemConfiguration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Range;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SystemConfiguration extends AbstractEntity {
+
+	// ------------------------------------------------------------------
+	private static final long serialVersionUID = 1L;
+	// ------------------------------------------------------------------
+	
+	
+	@NotBlank
+	protected String acceptedCurrencies;
+	
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{3}$")
+	protected String systemCurrency;
+	
+	protected String spamTerm;
+	
+	protected String spamWeight;
+	
+	@Range(min=0,max=1)
+	protected double spamThreshold;
+	
+	
+}
