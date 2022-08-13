ad44c87c00632aabdf6abc5d80d11e8f1a4f043f
JC <juamorper2@alum.us.es>
2022-08-13 19:11:17 +0200

    Arreglo

--- src/main/java/acme/features/epicure/finedish/FineDishEpicureController.java
+++ src/main/java/acme/features/epicure/finedish/FineDishEpicureController.java
@@ -30,7 +30,7 @@ public class FineDishEpicureController extends AbstractController<Epicure,FineDi
 		super.addCommand("list", this.listService);
 		super.addCommand("show", this.showService);
 		super.addCommand("create", this.createService);
-		super.addCommand("update", this.createService);
+		super.addCommand("update", this.updateService);
 		
 	}
 
