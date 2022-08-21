ca8e1295bac0d9c631a955b7bfbf732c7c673fad
JC <juamorper2@alum.us.es>
2022-08-21 17:56:53 +0200

    MoneyExchange implementado en finedish chef y epicure

--- src/main/java/acme/features/epicure/finedish/FineDishEpicureShowService.java
+++ src/main/java/acme/features/epicure/finedish/FineDishEpicureShowService.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.finedish.FineDish;
+import acme.entities.moneyExchange.MoneyExchange;
+import acme.features.authenticated.moneyExchange.AuthenticatedMoneyExchangePerformService;
 import acme.features.authenticated.systemConfiguration.AuthenticatedSystemConfigurationRepository;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
+import acme.framework.datatypes.Money;
 import acme.framework.services.AbstractShowService;
 import acme.roles.Epicure;
 
@@ -36,6 +39,9 @@ public class FineDishEpicureShowService implements AbstractShowService<Epicure,F
 		assert request != null;
 		assert entity != null;
 		assert model != null;
+		
+		final Money newBudget = this.moneyExchangeFineDish(entity);
+		model.setAttribute("newBudget", newBudget);
 	
 
 		request.unbind(entity, model, "status", "code", "request", "budget", "creationMoment", "startDate", "endDate", "moreInfo", "published");
@@ -58,6 +64,29 @@ public class FineDishEpicureShowService implements AbstractShowService<Epicure,F
 		final int id = request.getModel().getInteger("id");
 		return  this.repository.findOneFineDishById(id);
 	
+	}
+	
+	public Money moneyExchangeFineDish(final FineDish f) {
+		final String itemCurrency = f.getBudget().getCurrency();
+	
+		final AuthenticatedMoneyExchangePerformService moneyExchange = new AuthenticatedMoneyExchangePerformService();
+		final String systemCurrency = this.systemConfigRepository.findSystemConfiguration().getSystemCurrency();
+		final Double conversionAmount;
+		
+		if(!systemCurrency.equals(itemCurrency)) {
+			MoneyExchange conversion;
+			conversion = moneyExchange.computeMoneyExchange(f.getBudget(), systemCurrency);
+			conversionAmount = conversion.getTarget().getAmount();	
+		}
+		else {
+			conversionAmount = f.getBudget().getAmount();
+		}
+		
+		final Money newBudget = new Money();
+		newBudget.setAmount(conversionAmount);
+		newBudget.setCurrency(systemCurrency);
+		
+		return newBudget;
 	}
 
 }
