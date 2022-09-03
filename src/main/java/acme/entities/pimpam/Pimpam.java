a0d5c6f7078421d78a9f29f04e405fe3da839e4e
frabotrom <franbotello70@gmail.com>
2022-09-03 20:30:36 +0200

    CC Task-1: Pimpam created and linked to Thing

---
+++ src/main/java/acme/entities/pimpam/Pimpam.java
@@ -0,0 +1,77 @@
+package acme.entities.pimpam;
+
+import java.time.Period;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Pimpam extends AbstractEntity {
+	
+	// Serialisation identifier --------------------------------------
+
+	protected static final long serialVersionUID = 1L;
+
+	// Attributes ----------------------------------------------------
+	
+	@NotBlank
+	@Pattern(regexp = "^\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")
+	// pattern “PATTERN”, where “yy”, “mm”, and “dd” refer to the year, month, and day when the PIMPAM is created
+	protected String 					code;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date 						instantiationMoment;
+	
+	
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String 					title;
+	
+	@NotBlank
+	@Length(min = 1, max = 255)
+	protected String 					description;
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date						startDate;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date						endDate;
+	
+	@NotNull
+	@Valid
+	protected Money						budget;
+	
+	@URL
+	protected String 					link;
+	
+	// Derived attributes -----------------------------------------------------
+	@Transient
+	public Period period() {
+		final Integer result = (int) (this.endDate.getTime() - this.startDate.getTime()); 
+		final Integer resultDays = result/(1000*60*60*24);
+		return Period.ofDays(resultDays);
+	}
+	
+	
+}
--- src/main/java/acme/entities/thing/Thing.java
+++ src/main/java/acme/entities/thing/Thing.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.pimpam.Pimpam;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import acme.roles.Chef;
@@ -62,6 +64,10 @@ public class Thing extends AbstractEntity{
 	@Valid
 	@ManyToOne(optional=false)
 	protected Chef chef;
+
+	
+	@OneToOne(optional = true)
+	protected Pimpam pimpam;
 	
 	
 }
